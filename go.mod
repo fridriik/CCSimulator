4c335de841225da7f5892192cb3dd899e493b96e
Agustina.Insua <aguss.insua99@gmail.com>
2022-06-03 21:36:18 -0300

    se crearon las tablas

---
+++ go.mod
@@ -0,0 +1,5 @@
+module tp
+
+go 1.17
+
+require github.com/lib/pq v1.10.6
--- tp.go
+++ tp.go
@@ -6,9 +6,12 @@ import (
 	_"github.com/lib/pq"
 	"log"
 )
+
 type cliente struct {
-	nro_cliente int
-	nombre, apellido, domicilio string
+	nrocliente int
+    nombre string
+    apellido string
+	domicilio string
 	telefono [12] rune
 }
 
@@ -105,7 +108,7 @@ func createDatabase(){
 	if err != nil {
 		log.Fatal(err)
 	}
-		}
+}
 		
 func deleteDatabase(){
 	db, err := sql.Open("postgres", "user=postgres host=localhost dbname=postgres sslmode=disable")
@@ -138,9 +141,126 @@ func main(){
     domicilio text,
     telefono char(12)
     )`)
+
+    if err !=nil {
+    	log.Fatal(err)
+    }
+
+    _, err = db.Exec(`create table tarjeta(
+    nrotarjeta char(16),
+    nrocliente int,
+    validadesde char(6), 
+    validahasta char(6),
+    codseguridad char(4),
+    limitecompra decimal(8,2),
+    estado char(10) 
+    )`)
     
     if err !=nil {
     	log.Fatal(err)
     }
+    
+    _, err = db.Exec(`create table comercio(
+    nrocomercio int,
+    nombre text,
+    domicilio text,
+    codigopostal char(8),
+    telefono char(12)
+    )`)
+    
+     if err !=nil {
+    	log.Fatal(err)
+    }
+    
+    _, err = db.Exec(`create table compra(
+    nrooperacion int,
+    nrotarjeta char(16),
+    nrocomercio int,
+    fecha timestamp,
+    monto decimal(7,2),
+    pagado boolean
+    )`)
+    
+    if err !=nil {
+    	log.Fatal(err)
+    }
+    
+    _, err = db.Exec(`create table rechazo(
+    nrorechazo int,
+    nrotarjeta char(16),
+    nrocomercio int,
+    fecha timestamp,
+    monto decimal(7,2),
+    motivo text
+    )`)
+
+    if err !=nil {
+    log.Fatal(err)
+    }
+
+    _, err = db.Exec(`create table cierre(
+    año int,
+    mes int,
+    terminacion int,
+    fechainicio date,
+    fechacierre date,
+    fechavto date
+    )`)
+
+    if err !=nil {
+    log.Fatal(err)
+    }
+    
+    _, err = db.Exec(`create table cabecera(
+    nroresumen int,
+    nombre text,
+    apellido text,
+    domicilio text,
+    nrotarjeta char(16),
+    desde date,
+    hasta date,
+    vence date,
+    total decimal(8,2)
+    )`)   
+
+    if err !=nil {
+    log.Fatal(err)
+    }
+
+    _, err = db.Exec(`create table detalle(
+    nroresumen int,
+    nrolinea int,
+    fecha date,
+    nombrecomercio text,
+    monto decimal(7,2)
+    )`)     
+    
+    if err !=nil {
+    log.Fatal(err)
+    }    
+
+    _, err = db.Exec(`create table alerta(
+    nroalerta int,
+    nrotarjeta char(16),
+    fecha timestamp,
+    nrorechazo int,
+    codalerta int,
+    descripcion text
+    )`)
+
+    if err !=nil {
+    log.Fatal(err)
+    } 
+
+    _, err = db.Exec(`create table consumo(
+    nrotarjeta char(16),
+    codseguridad char(4),
+    nrocomercio int,
+    monto decimal(7,2)
+    )`)
+
+    if err !=nil {
+    log.Fatal(err)
+    } 
     
 }
