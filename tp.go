b58fc005387d6178b809f40f364cc8dfdb7df876
frderickk <federicofarias@outlook.com>
2022-06-03 21:16:19 -0300

    se completaron los structs

--- tp.go
+++ tp.go
@@ -12,6 +12,87 @@ type cliente struct {
 	telefono [12] rune
 }
 
+type tarjeta struct {
+    nrotarjeta [16] rune
+    nrocliente int
+    validadesde [6] rune
+    validadhasta [6] rune
+    codseguridad [4] rune
+    limitecompra float32    //decimal(8,2)
+    estado [10] rune
+}
+
+type comercio struct {
+    nrocomercio int
+    nombre string
+    domicilio string
+    codigopostal [8] rune
+    telefono [12] rune
+}
+
+type compra struct {
+    nrooperacion int
+    nrotarjeta [16] rune
+    nrocomercio int
+    fecha string    //time.Time
+    monto float32   //decimal(7,2)
+    pagado bool
+}
+
+type rechazo struct {
+    nrorechazo int
+    nrotarjeta [16] rune
+    nrocomercio int
+    fecha string    //time.Time
+    monto float32   //decimal(7,2)
+    motivo string
+}
+
+type cierre struct {
+    anio int
+    mes int
+    terminacion int
+    fechainicio string  //time.Time
+    fechacierre string  //time.Time
+    fechavto string   //time.Time
+}
+
+type cabecera struct {
+    nroresumen int
+    nombre string
+    apellido string
+    domicilio string
+    nrotarjeta [16] rune
+    desde string    //time.Time
+    hasta string    //time.Time
+    vence string    //time.Time
+    total float32   //decimal(8,2)
+}
+
+type detalle struct {
+    nroresumen int
+    nrolinea int
+    fecha string    //time.Time
+    nombrecomercio string
+    monto float32   //decimal(7,2)
+}
+
+type alerta struct {
+    nroalerta int
+    nrotarjeta [16] rune
+    fecha string    //time.Time
+    nrorechazo int
+    codalerta int   //0: rechazo, 1: compra 1 min, 5: compra 5 min, 32: limite
+    descripcion string
+}
+
+type consumo struct {
+    nrotarjeta [16] rune
+    codseguridad [4] rune
+    nrocomercio int
+    monto float32   //decimal(7,2)
+}
+
 func createDatabase(){
 
 	db, err := sql.Open("postgres", "user=postgres host=localhost dbname=postgres sslmode=disable")
