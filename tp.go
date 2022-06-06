f0fda5bf2e77279aec093e197da76ee5705aabc9
Lautaro Avalos <lilo@hawaii.my.domain>
2022-06-06 04:32:07 -0300

    Refactor crearTablas()

--- tp.go
+++ tp.go
@@ -125,7 +125,85 @@ func deleteDatabase(){
 		log.Fatal(err)
 	}
 }
+func crearTablas(db1 *sql.DB){
 
+	db := db1
+	var err error
+	
+	_, err = db.Exec(
+ `create table cliente(
+							nrocliente int,
+							nombre text,
+    						apellido text,
+    						domicilio text,
+    						telefono char(12));
+  create table tarjeta(
+   							nrotarjeta char(16),
+							nrocliente int,
+							validadesde char(6), 
+							validahasta char(6),
+							codseguridad char(4),
+							limitecompra decimal(8,2),
+							estado char(10));
+  create table comercio(
+   							nrocomercio int,
+   							nombre text,
+    						domicilio text,
+    						codigopostal char(8),
+    						telefono char(12));
+  create table compra(
+  							nrooperacion int,
+    						nrotarjeta char(16),
+    						nrocomercio int,
+    						fecha timestamp,
+    						monto decimal(7,2),
+    						pagado boolean);
+  create table rechazo(
+  							nrorechazo int,
+    						nrotarjeta char(16),
+    						nrocomercio int,
+    						fecha timestamp,
+    						monto decimal(7,2),
+    						motivo text);
+  create table cierre(
+  							año int,
+    						mes int,
+    						terminacion int,
+    						fechainicio date,
+    						fechacierre date,
+							fechavto date);
+  create table cabecera(
+  							nroresumen int,
+    						nombre text,
+    						apellido text,
+    						domicilio text,
+    						nrotarjeta char(16),
+    						desde date,
+    						hasta date,
+    						vence date,
+    						total decimal(8,2));   
+  create table detalle(
+ 							nroresumen int,
+    						nrolinea int,
+    						fecha date,
+    						nombrecomercio text,
+    						monto decimal(7,2));     
+  create table alerta(
+  							nroalerta int,
+    						nrotarjeta char(16),
+    						fecha timestamp,
+    						nrorechazo int,
+    						codalerta int,
+    						descripcion text);
+  create table consumo(
+   							nrotarjeta char(16),
+    						codseguridad char(4),
+    						nrocomercio int,
+    						monto decimal(7,2));`)
+    if err !=nil {
+    	log.Fatal(err)
+    } 
+}
 
 func definirPksYFks(db1 *sql.DB){
 
@@ -426,105 +504,7 @@ func main(){
 	}
 	defer db.Close()
 
-	 _, err = db.Exec(`create table cliente(nrocliente int,
-    										nombre text,
-    										apellido text,
-    										domicilio text,
-    										telefono char(12))`)
-    if err !=nil {
-    	log.Fatal(err)
-    }
-
-    _, err = db.Exec(`create table tarjeta(nrotarjeta char(16),
-    									   nrocliente int,
-    									   validadesde char(6), 
-    									   validahasta char(6),
-    									   codseguridad char(4),
-    									   limitecompra decimal(8,2),
-    									   estado char(10))`)
-    if err !=nil {
-    	log.Fatal(err)
-    }
-    
-    _, err = db.Exec(`create table comercio(nrocomercio int,
-    										nombre text,
-    										domicilio text,
-    										codigopostal char(8),
-    										telefono char(12))`)
-     if err !=nil {
-    	log.Fatal(err)
-    }
-    
-    _, err = db.Exec(`create table compra(nrooperacion int,
-    									  nrotarjeta char(16),
-    									  nrocomercio int,
-    									  fecha timestamp,
-    									  monto decimal(7,2),
-    									  pagado boolean)`)
-    if err !=nil {
-    	log.Fatal(err)
-    }
-    
-    _, err = db.Exec(`create table rechazo(nrorechazo int,
-    									   nrotarjeta char(16),
-    									   nrocomercio int,
-    									   fecha timestamp,
-    									   monto decimal(7,2),
-    									   motivo text)`)
-    if err !=nil {
-    	log.Fatal(err)
-    }
-
-    _, err = db.Exec(`create table cierre(año int,
-    									  mes int,
-    									  terminacion int,
-    									  fechainicio date,
-    									  fechacierre date,
-    									  fechavto date)`)
-    if err !=nil {
-    	log.Fatal(err)
-    }
-    
-    _, err = db.Exec(`create table cabecera(nroresumen int,
-    										nombre text,
-    										apellido text,
-    										domicilio text,
-    										nrotarjeta char(16),
-    										desde date,
-    										hasta date,
-    										vence date,
-    										total decimal(8,2))`)   
-    if err !=nil {
-    	log.Fatal(err)
-    }
-
-    _, err = db.Exec(`create table detalle(nroresumen int,
-    									   nrolinea int,
-    									   fecha date,
-    									   nombrecomercio text,
-    									   monto decimal(7,2))`)     
-    if err !=nil {
-    	log.Fatal(err)
-    }    
-
-    _, err = db.Exec(`create table alerta(nroalerta int,
-    									  nrotarjeta char(16),
-    									  fecha timestamp,
-    									  nrorechazo int,
-    									  codalerta int,
-    									  descripcion text)`)
-    if err !=nil {
-    	log.Fatal(err)
-    } 
-
-    _, err = db.Exec(`create table consumo(nrotarjeta char(16),
-    									   codseguridad char(4),
-    									   nrocomercio int,
-    									   monto decimal(7,2))`)
-    if err !=nil {
-    	log.Fatal(err)
-    } 
-
+	crearTablas(db)
     definirPksYFks(db)
     cargarClientes(db)
 	cargarComercio(db)
